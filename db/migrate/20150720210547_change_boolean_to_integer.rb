ed2b00e7a046f9764c5f2d4e66bec1d8d5110bff
gideon <gideon@blend.la>
2015-07-25 16:38:09 -0700

    fixing PG pt 2

--- db/migrate/20150720210547_change_boolean_to_integer.rb
+++ db/migrate/20150720210547_change_boolean_to_integer.rb
@@ -1,6 +1,6 @@
 class ChangeBooleanToInteger < ActiveRecord::Migration
   def change
     change_column(:track_likes, :liked, 'integer USING CAST(liked AS integer)')
-    change_column(:users, :imported, 'integer USING CAST(liked AS integer)')
+    change_column(:users, :imported, 'integer USING CAST(imported AS integer)')
   end
 end
